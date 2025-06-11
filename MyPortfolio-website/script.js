706d3c81ca8696b2bf3e246ce8e7f5d8ce63a087
jayeshnikhade <jayeshnikhade37@gmail.com>
2025-06-11 15:24:50 -0400

    Add files via upload

---
+++ MyPortfolio-website/script.js
@@ -0,0 +1,23 @@
+let github = document.querySelector("#btn2");
+
+github.addEventListener("click", function () {
+    window.open("https://github.com/jayeshnikhade","_blank")
+});
+
+let linkdin = document.querySelector("#Linkdin");
+
+linkdin.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/jayesh-nikhade-0997882a6/", "_blank")
+});
+
+let opengithub = document.querySelector("#github");
+
+opengithub.addEventListener("click", function () {
+    window.open("https://github.com/jayeshnikhade", "_blank")
+});
+
+let instagram = document.querySelector("#insta");
+
+instagram.addEventListener("click", function () {
+    window.open("https://www.instagram.com/jayeshnikhade_official/", "_blank")
+});
